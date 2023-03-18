908f02d832f238ec36cc491d093e8b0b4fd2191b
Alex-Pychyl <alex.pychyl@gmail.com>
2023-03-18 16:31:03 -0400

    fixed bug

--- RpiSender/rpisender.cpp
+++ RpiSender/rpisender.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <wiringPi.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -12,34 +13,38 @@ const char codes[16][100] = {"It's morbin' time", "Beam me up, Scotty", "Power u
 int main() {
     int command = -1;
     char c;
-    int instructions[6] = {0};
+    int instructions[6] = {0, 0, 0, 0, 0, 0};
     
     wiringPiSetup();
     pinMode(outputPin, OUTPUT);
     pinMode(ledPin, OUTPUT);
     
-    printf("Enter number for command to send:\n\n");
-    for (int i = 0; i < 16; i++) {
-        printf("(%i) --> %s\n", i, codes[i]);
-    }
-    while ((!scanf("%i", &command))||command<0||command>15) {
-        while((c = getchar()) != '\n' && c != EOF);
-        printf("Invalid, try again: ");
-    }
-    
-    for (int i = 0; i < 4; i++) {
-        if (command-(1<<(3-i))>=0) {
-            instructions[i+1] = 1;
-            command-=1<<(3-i);
+    while (1) {
+        system("clear");
+        printf("Enter number for command to send or -1 to quit:\n\n");
+        for (int i = 0; i < 16; i++) {
+            printf("(%i) --> %s\n", i, codes[i]);
         }
-    }
+        while ((!scanf("%i", &command))||command<-1||command>15) {
+            while((c = getchar()) != '\n' && c != EOF);
+            printf("Invalid, try again: ");
+        }
+        
+        if (command==-1) break;
+        
+        for (int i = 0; i < 4; i++) {
+            if (command-(1<<(3-i))>=0) {
+                instructions[i+1] = 1;
+                command-=1<<(3-i);
+            } else instructions[i+1] = 0;
+        }
 
-    for (int i = 0; i < 6; i++) {
-        digitalWrite(outputPin, instructions[i]);
-        digitalWrite(ledPin, instructions[i]);
-        printf("%i", instructions[i]);
-        delay(cycle);
+        for (int i = 0; i < 6; i++) {
+            digitalWrite(outputPin, instructions[i]);
+            digitalWrite(ledPin, instructions[i]);
+            delay(cycle);
+        }
+        digitalWrite(ledPin, 0);
+        digitalWrite(outputPin, 1);
     }
-    digitalWrite(ledPin, 0);
-    digitalWrite(outputPin, 1);
 }
